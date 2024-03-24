aac8cbf6213b17ba4fc429bcbe42656ef04530b2
Fujijared2810 <scorpeejared2@gmail.com>
2024-03-24 19:09:03 +0800

    Post page done

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -54,7 +54,7 @@ const getPost = async (req, res) => {
       return res.status(404).json({ error: "post not found." });
     }
 
-    res.status(200).json({ post });
+    res.status(200).json(post);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
