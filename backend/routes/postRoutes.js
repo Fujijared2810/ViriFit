90271c2e6330a9584cbf548652e7d21111543017
Fujijared2810 <scorpeejared2@gmail.com>
2024-03-24 15:11:23 +0800

    like and unlike

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,13 @@
-import express from 'express';
-import { createPost, deletePost, getFeedPosts, getPost, likeUnlikePost, replyToPost } from '../controllers/postController.js';
-import protectRoute from '../middlewares/protectRoute.js';
+import express from "express";
+import {
+  createPost,
+  deletePost,
+  getFeedPosts,
+  getPost,
+  likeUnlikePost,
+  replyToPost,
+} from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
@@ -8,8 +15,7 @@ router.get("/feed", protectRoute, getFeedPosts);
 router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
-router.post("/like/:id", protectRoute, likeUnlikePost);
-router.post("/reply/:id", protectRoute, replyToPost);
-
+router.put("/like/:id", protectRoute, likeUnlikePost);
+router.put("/reply/:id", protectRoute, replyToPost);
 
 export default router;
