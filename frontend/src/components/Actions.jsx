f902a69b24e35bb2c03caa113be9ec6008e12cd2
Fujijared2810 <scorpeejared2@gmail.com>
2024-03-26 23:58:29 +0800

    star added

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -120,16 +120,17 @@ const Actions = ({ post }) => {
       <Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
         <svg
           aria-label="Like"
-          color={liked ? "rgb(237, 73, 86)" : ""}
-          fill={liked ? "rgb(237, 73, 86)" : "transparent"}
+          color={liked ? "rgb(255, 215, 0)" : ""}
+          fill={liked ? "rgb(255, 215, 0)" : "transparent"}
           height="19"
           role="img"
           viewBox="0 0 24 22"
           width="20"
           onClick={handleLikeAndUnlike}
         >
+          <title>Like</title>
           <path
-            d="M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z"
+            d="M22,10.1c0.1-0.5-0.3-1.1-0.8-1.1l-5.7-0.8L12.9,3c-0.1-0.2-0.2-0.3-0.4-0.4C12,2.3,11.4,2.5,11.1,3L8.6,8.2L2.9,9C2.6,9,2.4,9.1,2.3,9.3c-0.4,0.4-0.4,1,0,1.4l4.1,4l-1,5.7c0,0.2,0,0.4,0.1,0.6c0.3,0.5,0.9,0.7,1.4,0.4l5.1-2.7l5.1,2.7c0.1,0.1,0.3,0.1,0.5,0.1v0c0.1,0,0.1,0,0.2,0c0.5-0.1,0.9-0.6,0.8-1.2l-1-5.7l4.1-4C21.9,10.5,22,10.3,22,10.1z"
             stroke="currentColor"
             strokeWidth="2"
           ></path>
@@ -154,9 +155,6 @@ const Actions = ({ post }) => {
             strokeWidth="2"
           ></path>
         </svg>
-
-        <RepostSVG />
-        <ShareSvg />
       </Flex>
 
       <Flex gap={2} alignItems={"center"}>
@@ -203,55 +201,55 @@ const Actions = ({ post }) => {
 
 export default Actions;
 
-const RepostSVG = () => {
-  return (
-    <svg
-      aria-label="Repost"
-      color="currentColor"
-      fill="currentColor"
-      height="20"
-      role="img"
-      viewBox="0 0 24 24"
-      width="20"
-    >
-      <title>Repost</title>
-      <path
-        fill=""
-        d="M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z"
-      ></path>
-    </svg>
-  );
-};
+// const RepostSVG = () => {
+//   return (
+//     <svg
+//       aria-label="Repost"
+//       color="currentColor"
+//       fill="currentColor"
+//       height="20"
+//       role="img"
+//       viewBox="0 0 24 24"
+//       width="20"
+//     >
+//       <title>Repost</title>
+//       <path
+//         fill=""
+//         d="M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z"
+//       ></path>
+//     </svg>
+//   );
+// };
 
-const ShareSvg = () => {
-  return (
-    <svg
-      aria-label="Share"
-      color=""
-      fill="rgb(243, 245, 247)"
-      height="20"
-      role="img"
-      viewBox="0 0 24 24"
-      width="20"
-    >
-      <title>Share</title>
-      <line
-        fill="none"
-        stroke="currentColor"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        x1="22"
-        x2="9.218"
-        y1="3"
-        y2="10.083"
-      ></line>
-      <polygon
-        fill="none"
-        points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
-        stroke="currentColor"
-        strokeLinejoin="round"
-        strokeWidth="2"
-      ></polygon>
-    </svg>
-  );
-};
+// const ShareSvg = () => {
+//   return (
+//     <svg
+//       aria-label="Share"
+//       color=""
+//       fill="rgb(243, 245, 247)"
+//       height="20"
+//       role="img"
+//       viewBox="0 0 24 24"
+//       width="20"
+//     >
+//       <title>Share</title>
+//       <line
+//         fill="none"
+//         stroke="currentColor"
+//         strokeLinejoin="round"
+//         strokeWidth="2"
+//         x1="22"
+//         x2="9.218"
+//         y1="3"
+//         y2="10.083"
+//       ></line>
+//       <polygon
+//         fill="none"
+//         points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+//         stroke="currentColor"
+//         strokeLinejoin="round"
+//         strokeWidth="2"
+//       ></polygon>
+//     </svg>
+//   );
+// };
